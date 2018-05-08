58b2d4248180a28b18381f836233c2c787f00131
刘然 <liuran@liurans-MacBook-Pro.local>
2018-05-08 23:34:51 +0800

    修改web配置

--- src/main/java/com/demo/order/controller/TestController.java
+++ src/main/java/com/demo/order/controller/TestController.java
@@ -3,25 +3,25 @@ package com.demo.order.controller;
 import com.demo.order.service.TestService;
 import com.demo.order.vo.TestDemoVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "demo/test")
+@RequestMapping(value = "/demo/test")
 public class TestController {
 
     @Autowired
     private TestService service;
 
-    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    @RequestMapping(value = "", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE } )
     public void test(@RequestBody TestDemoVo demo){
         System.out.printf(demo.getName());
-        service.testService();
     }
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public void get(){
         service.testService();
     }
